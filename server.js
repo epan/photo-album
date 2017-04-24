d27de4495c90082a1f5978ba4664323d2e600533
Eric Pan <epan@users.noreply.github.com>
2017-04-23 20:50:42 -0700

    Set up server to listen good

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+const IP = '127.0.0.1';
+const PORT = process.env.PORT || 1337;
+
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+app.listen(PORT, () => { console.log(`Listening on ${IP}:${PORT}`); });
