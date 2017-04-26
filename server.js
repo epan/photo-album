a86edb838148d8a2c0b36b132ad40e7e292907e9
Eric Pan <epan@users.noreply.github.com>
2017-04-26 10:03:46 -0700

    Serve static index page by fixing routes and static

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const db = require('./db/db-config');
+const path = require('path');
 
 const app = express();
 const IP = '127.0.0.1';
@@ -9,6 +10,12 @@ const PORT = process.env.PORT || 1337;
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+app.use(express.static(__dirname + '/client'));
 
-app.listen(PORT, () => { console.log(`Listening on ${IP}:${PORT}`); });
+app.listen(PORT, () => { 
+  console.log(`Listening on ${IP}:${PORT}`); 
+});
 
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/index.html'));
+// });
